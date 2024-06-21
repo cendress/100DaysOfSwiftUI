88fc58e9cd20aa1ef8cee3515ecc902ff45fc025
Christopher Endress <95492688+cendress@users.noreply.github.com>
2024-06-21 13:51:53 -0400

    Add project6

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Christopher Endress on 6/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       Text("Hello")
+    }
+}
+
+#Preview {
+    ContentView()
+}
